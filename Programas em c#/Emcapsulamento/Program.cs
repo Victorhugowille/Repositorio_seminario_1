a52af2ac414aa5f353ffe98ab3c0f9dbfa4b7ff8
Victorhugowille <victorhugowille@gmail.com>
2025-05-08 14:01:20 -0300

    Add files via upload

---
+++ Programas em c#/Emcapsulamento/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+    public static void Main()
+    {
+        var carro = new Carro();
+        carro.Speed = 40;
+        carro.Aceleracao();
+
+        Console.WriteLine ($"Speed: {carro.Speed}");
+    }
+}
+public class Carro
+{
+    private int _speed;
+    public int Speed 
+    { 
+             get{return _speed;}
+             set{_speed = value;} 
+    }
+    public void Aceleracao()
+    {
+        _speed += 40;
+    }
+}
